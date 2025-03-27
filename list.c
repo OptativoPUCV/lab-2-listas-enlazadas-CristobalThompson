e95b2ca870b3399ccde2adb4aa92807a50185d7f
Your Name <you@example.com>
2025-03-27 10:08:22 -0300

    new try

--- list.c
+++ list.c
@@ -29,7 +29,7 @@ Node * createNode(void * data) {
 }
 
 List * createList() {
-    List* L = (List *) malloc(sizeof(Lista));
+    List* L = (List *) malloc(sizeof(List));
     L->head = NULL;
     L->tail = NULL;
     L->current = NULL;
